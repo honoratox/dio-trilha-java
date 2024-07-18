3b2b4c13e15e06f77c3d388ac392eb5af0678097
Pedro Honorato <111890943+honoratox@users.noreply.github.com>
2024-07-18 13:16:35 -0300

    Add files via upload

---
+++ estruturas-condicionais/switch-case/src/SistemaMedida.java
@@ -0,0 +1,21 @@
+//switch case de siglas de medidas pequeno, medio e grande
+public class SistemaMedida {
+    public static void main(String[]args){
+
+        String sigla = "M";
+        switch(sigla){
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("MEDIO");
+                break;
+            }
+            case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+        }
+    }
+}
